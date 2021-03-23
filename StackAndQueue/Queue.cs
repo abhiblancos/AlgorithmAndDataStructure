f23644fedecf8b0addc1a382cee9c751513bd2fe
Abhishek Deshmukh <abhishek-deshmukh@hotmail.com>
2021-03-23 18:06:21 +0530

    Queue

---
+++ StackAndQueue/Queue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+	public class Queue
+	{
+		private Node first { get; set; }
+
+		private Node last { get; set; }
+
+		private int length { get; set; }
+
+		public Queue()
+		{
+			this.first = null;
+			this.last = null;
+			this.length = 0;
+		}
+
+		public void Enqueue(int value)
+		{
+			Node newNode = new(value);
+			if(length == 0)
+			{
+				this.first = newNode;
+				this.last = newNode;
+			}
+			else
+			{				
+				this.last.next = newNode;
+				this.last = newNode;				
+			}
+
+		}
+
+		public void Dequeue()
+		{
+			if (this.first == null)
+				return;
+
+			if(this.length == 0)
+			{
+				this.last = null;
+			}
+
+			Node holdNode = this.first;
+			this.first = this.first.next;
+
+			this.length--;
+
+
+		}
+	}
+}
